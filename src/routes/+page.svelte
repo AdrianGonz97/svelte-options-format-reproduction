b56bd9130e9a9f424b17c2bf90dd3223c20f634c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-22 18:59:05 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let hello = "world";
+</script>
+
+<svelte:options />
+
+<!-- Save to format on save to see issue -->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
