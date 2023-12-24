d021c3064f46544553fdc639f898c97a79f7d98b
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-23 22:42:14 -0500

    clear the fluff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,7 @@
 
 <svelte:options />
 
-<!-- Save file to format on save to see issue -->
+<!-- Format with the default formatter to demonstrate issue -->
 
 <!-- Interestingly, the issue doesn't appear if there's a comment above the element -->
 <!-- <svelte:options /> -->
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
