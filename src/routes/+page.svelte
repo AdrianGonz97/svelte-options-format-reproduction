34222f6b41718488140fc072c3e0cd80499ed18c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-23 22:20:44 -0500

    comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
 
 <svelte:options />
 
-<!-- Save to format on save to see issue -->
+<!-- Save file to format on save to see issue -->
+
+<!-- Interestingly, the issue doesn't appear if there's a comment above the element -->
+<!-- <svelte:options /> -->
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
